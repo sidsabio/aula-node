f203d932cccbffacd94309cb5d129a1e5cad1c79
sidsabio <sidneymg.13@gmail.com>
2024-11-21 17:47:19 -0300

    API Node com Postgres

---
+++ db.js
@@ -0,0 +1,24 @@
+// import "dotenv/config";
+// import postgres from "postgres";
+
+// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+// export const sql = postgres(URL, { ssl: "require" });
+
+import "dotenv/config";
+import http from "http";
+import { neon } from "@neondatabase/serverless";
+
+export const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
---
+++ server.js
@@ -0,0 +1,63 @@
+// import { createServer } from "node:http";
+
+// const server = createServer((request, response) => {
+//   response.write('Teste');
+//   return response.end();
+// });
+
+// server.listen(3333);
+
+import { fastify } from "fastify";
+// import { DatabaseMemory } from "./database-memory.js";
+import { DatabasePostgres } from "./database-postgres.js";
+
+const server = fastify();
+
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
+
+server.post("/videos", async (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  await database.create({
+    title: title,
+    description: description,
+    duration: duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.get("/videos", async (request) => {
+  const search = request.query.search;
+
+  const videos = await database.list(search);
+
+  return videos;
+});
+
+server.put("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+
+  const { title, description, duration } = request.body;
+
+  await database.update(videoId, {
+    title: title,
+    description: description,
+    duration: duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return reply.status(204).send();
+});
+
+server.listen({
+  port: process.env.PORT ?? 3333,
+});
